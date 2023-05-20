2e97c9f97ea43584ea51c9325cbf8f57464b34a5
Rajs2001 <88596141+Rajs2001@users.noreply.github.com>
2023-05-20 15:24:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Toggle navbar on burger icon click
+
+const burger = document.querySelector('.burger');
+
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+
+  nav.classList.toggle('nav-active');
+
+  // Animate burger icon
+
+  burger.classList.toggle('line1');
+
+  burger.classList.toggle('line2');
+
+  burger.classList.toggle('line3');
+
+});
 
